49a6c279a88a3efbba6070a0f3a1c24a7545195c
Guilherme Batista <guilhermemalka6@gmail.com>
2022-03-31 17:13:24 -0300

    initial commit

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,30 @@
+import { Negociacoes } from './../models/negociacoes.js';
+import { Negociacao } from './../models/negociacao.js';
+export class NegociacaoController {
+    constructor() {
+        this.negociacoes = new Negociacoes();
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoes.lista();
+        console.log(this.negociacoes.lista());
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseInt(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
